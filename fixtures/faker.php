b3f249b4e97577a051f7544f50ff19ecbabc0eff
Volodymyr Klekot <Volodymyr_Klekot@epam.com>
2018-04-05 17:37:31 +0300

    updated faker

--- fixtures/faker.php
+++ fixtures/faker.php
@@ -5,12 +5,12 @@ use app\src\Entity\Posts;
 use Faker\Factory;
 
 $faker = Factory::create();
-$faker->seed(123);
 
 for ($i = 1; $i < 10; $i++) {
     $posts = new Posts();
     $posts->setTitle($faker->title);
     $posts->setBody($faker->text);
+    $posts->setUserId($faker->numberBetween(1,10));
 
     $entityManager->persist($posts);
     $entityManager->flush();
